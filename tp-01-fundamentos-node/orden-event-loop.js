294c242edc709653a63b690cfbeef2a6d2611eb9
Leandro Matias Miguel <leomiguel296@gmail.com>
2026-08-24 12:17:32 -0300

    orden-event-listo

--- tp-01-fundamentos-node/orden-event-loop.js
+++ tp-01-fundamentos-node/orden-event-loop.js
@@ -1,7 +1,9 @@
-console.log("1.Comienza el programa");
+console.log("1. Comienza el programa");
 
 setTimeout(() => {
-    console.log("3.Se ejecuta la tarea programada");
+    console.log("3. Se ejecuta la tarea programada");
     }, 0);
 
 console.log("2. Termina el codigo principal");
+
+
